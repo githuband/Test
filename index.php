7af936d47b30681fdc2fc30e0e1c922b9ccf580b
githuband <githuband@gmail.com>
2015-11-28 23:33:56 +0100

    master clean

--- index.php
+++ index.php
@@ -13,52 +13,7 @@
 
 	<body>
 
-    <div class="style">
-        Pellentesque habitant morbi tristique senectus et
-        Pellentesque habitant morbi tristique senectus et
-        Pellentesque habitant morbi tristique senectus et
-        Pellentesque habitant morbi tristique senectus et
-        Pellentesque habitant morbi tristique senectus et
-        Pellentesque habitant morbi tristique senectus et
-        Pellentesque habitant morbi tristique senectus et
-        Pellentesque habitant morbi tristique senectus et
-        Pellentesque habitant morbi tristique senectus et
-        Pellentesque habitant morbi tristique senectus et
-        Pellentesque habitant morbi tristique senectus et
-    </div>
-
-    <h1 class="my your">My</h1>
-    <h1 class="my">My</h1>
-    <h1 class="your">My</h1>
-
-        <div>
-            <h1>Pellentesque habitant morbi tristique senectus et</h1>
-            <h2>Pellentesque habitant morbi tristique senectus et</h2>
-            <h3>Pellentesque habitant morbi tristique senectus et</h3>
-            <h4>Pellentesque habitant morbi tristique senectus et</h4>
-            <h5>Pellentesque habitant morbi tristique senectus et</h5>
-            <h6>Pellentesque habitant morbi tristique senectus et</h6>
-        </div>
-
-        <h1>Typography</h1>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
-        
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-
-        <h2>Typography</h2>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-        
-        <h3>Typography</h3>
-        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-
-<!--
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
--->
-        <script src="bower_components/jquery/dist/jquery.min.js"></script>
         <script src="js/code.js"></script>
 
 	</body> 
