afeea6f6bc6a2abe571702c40a110d09a904c0ce
githuband <githuband@gmail.com>
2015-11-29 18:55:17 +0100

    rebase 18:55

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
         <h1>Test</h1>
 
         <h2>Subtest</h2>
+        <h2>Subtest</h2>
+        <h2>Subtest</h2>
+        <h2>Subtest</h2>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="js/code.js"></script>
