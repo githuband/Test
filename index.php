16507bd1c6fe00b969bc3a2e589cfc55aa738f14
githuband <githuband@gmail.com>
2015-11-29 18:16:48 +0100

    mychange

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
         <h1>Test</h1>
 
-        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script> -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="js/code.js"></script>
 
 	</body> 
